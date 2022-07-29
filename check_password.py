a6aa34fa1ce53391914e4bdf66fb8d9b4090f3b7
Sally Pang <sallyshueyan.pang@my.jcu.edu.au>
2022-07-29 09:42:08 +0800

    Modify new password length criteria

--- check_password.py
+++ check_password.py
@@ -1,7 +1,7 @@
 def main():
     password = input("Enter password: ")
-    if len(password) < 8:
-        print("Not enough characters for new password")
+    if len(password) < 8 or len(password) > 20:
+        print("New password must have 8 to 20 characters!")
 
 
 main()
